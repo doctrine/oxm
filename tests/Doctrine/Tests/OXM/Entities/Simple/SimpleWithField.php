2fef41acc2fc266fb59e8841326100e22b2764a3
Richard Fullmer <rfullmer@printfulfillmentservices.com>
2011-06-10 11:31:25 -0400

    Moved all Simple entities into their own files for better class loading

--- tests/Doctrine/Tests/OXM/Entities/Simple/SimpleWithField.php
+++ tests/Doctrine/Tests/OXM/Entities/Simple/SimpleWithField.php
@@ -17,29 +17,7 @@
  * <http://www.doctrine-project.org>.
  */
 
-namespace Doctrine\Tests\OXM\Entities;
-
-/** @XmlEntity */
-class Simple { }
-    
-/** @XmlEntity */
-class SimpleCompound { }
+namespace Doctrine\Tests\OXM\Entities\Simple;
 
 /** @XmlRootEntity */
 class SimpleWithField { /** @XmlAttribute(type="integer", direct=true) @XmlId */ public $id; }
-
-/** @XmlMappedSuperclass */
-abstract class SimpleAbstractParent { /** @XmlText(type="string", direct=true) */public $other = "yes"; }
-
-/** @XmlMappedSuperclass */
-abstract class SimpleAbstractParentWithField { /** @XmlAttribute(type="integer", direct=true) */ private $id = 2; }
-
-/** @XmlEntity */
-class SimpleChild extends SimpleAbstractParent { }
-    
-/** @XmlEntity */
-class SimpleChildExtendsWithParentField extends SimpleAbstractParentWithField { }
-
-/** @XmlEntity */
-class SimpleChildExtendsWithChildField extends SimpleAbstractParent { /** @XmlAttribute(type="integer", direct=true) */ public $id; }
-
